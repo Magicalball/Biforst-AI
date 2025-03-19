3be677c16659d36ab3de2b7068f82b30353c246d
Magicalball <1355061380@qq.com>
2025-03-19 15:00:14 +0800

    初始化项目。 - 引入shadcn-ui。 - 引入Clerk - 项目初始化

---
+++ app/(routes)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+const DashboardPage = () => { 
+  return (
+    <div>
+      <h1>hello Dashboard Page (protected)</h1>
+      <Button size="lg">按钮</Button>
+    </div>
+  );
+}
+
+export default DashboardPage;
