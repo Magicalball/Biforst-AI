655a8b1bf6c9c0f0da48ea06c1b10df6ef7602b2
Magicalball <1355061380@qq.com>
2025-03-21 20:46:07 +0800

    更新了一些内容。 - 添加仪表板布局和导航栏组件。 - 更新.gitignore文件。 - 调整package.json依赖项

---
+++ app/(routes)/layout.tsx
@@ -0,0 +1,18 @@
+import Navbar from "@/components/navbar";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="relative h-full">
+      <div className="z-[80] hidden h-full bg-gray-900 md:fixed md:inset-y-0 md:flex md:w-72 md:flex-col">
+        <div>侧边栏</div>
+      </div>
+      <main className="md:pl-72">
+        <Navbar />
+        这里是内容。
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default DashboardLayout;
