0b2f6d0fddc9d4fbe23616a16623309362b70ff8
Magicalball <1355061380@qq.com>
2025-04-08 23:16:47 +0800

    添加图片生成器页面及部分功能

---
+++ app/(routes)/picture/page.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import axios from "axios";
+import * as z from "zod";
+import Markdown from "react-markdown";
+import { useState } from "react";
+import { ImageIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { formSchema } from "./constance";
+import { useRouter } from "next/navigation";
+import { ChatCompletionMessageParam } from "openai/resources/chat/index.mjs";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Heading } from "@/components/heading";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { EmptyPage } from "@/components/empty";
+import { Loader } from "@/components/loader";
+import { cn } from "@/lib/utils";
+import { BotHead } from "@/components/bothead";
+import { UserHead } from "@/components/userhead";
+
+const PicturePage = () => {
+  const router = useRouter();
+  const [images, setImages] = useState<string[]>([]); //图片数据
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+      amount: "1",
+      resolution: "512x512",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setImages([]);
+      const response = await axios.post("/api/picture", values);
+      const urls = response.data.map((image: { url: string }) => image.url);
+
+      form.reset(); //重置输入框
+    } catch (error: Error | unknown) {
+      //错误处理记得回头看一下
+      console.log(error);
+    } finally {
+      router.refresh(); //刷新页面
+    }
+  };
+
+  return (
+    <div>
+      <Heading
+        title="欢迎使用AI图片生成器"
+        description="在线AI生成图片：几秒钟内即可将文字转换为在线图像。"
+        icon={ImageIcon}
+        iconColor="text-pink-400"
+        bgColor="bg-pink-400/10"
+      />
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="grid w-full grid-cols-12 gap-2 rounded-lg border p-4 px-3 focus-within:shadow-sm md:px-6">
+              <FormField
+                name="prompt"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="描绘出梦想中的图片，比如“一个在月球上喝茶的猫”..."
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button
+                className="col-span-12 w-full lg:col-span-2"
+                disabled={isLoading}>
+                发送
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className="mt-6 space-y-4">
+          {isLoading && (
+            <div className="p-20">
+              <Loader />
+            </div>
+          )}
+          {images.length === 0 && !isLoading && <EmptyPage label="哇！被发现了^-^" />}
+          <div>123</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PicturePage;
