1db83875e0bcaed8ee2d18d9b016eadb0cba1eb5
Magicalball <1355061380@qq.com>
2025-04-21 20:26:30 +0800

    更新按钮样式

--- components/freecount.tsx
+++ components/freecount.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Max_FREE_COUNTS } from "@/constance";
 import { Progress } from "@/components/ui/progress";
+import { Button } from "@/components/ui/button";
+import { Zap } from "lucide-react";
 
 interface FreeCounterProps {
   apiLimitCount: number;
@@ -19,8 +21,8 @@ export const FreeCounter = ({ apiLimitCount = 0 }: FreeCounterProps) => {
   }
   return (
     <div className="px-3">
-      <Card className="flex items-center justify-center border-0 bg-white/10">
-        <CardContent className="py-6">
+      <Card className="p-x-4 flex items-center justify-center border-0 bg-white/10">
+        <CardContent className="py-1">
           <div className="mb-4 space-y-2 text-center text-sm text-white">
             <p>
               已用额度：{apiLimitCount} per / {Max_FREE_COUNTS} per
@@ -30,6 +32,12 @@ export const FreeCounter = ({ apiLimitCount = 0 }: FreeCounterProps) => {
               className="h-2"
             />
           </div>
+          <Button
+            className="w-full"
+            variant="premium">
+            解锁PLUS版本
+            <Zap className="ml-2 h-4 w-4" />
+          </Button>
         </CardContent>
       </Card>
     </div>
